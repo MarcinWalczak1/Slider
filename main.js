187152e6606e87281ae3bf9658c8738e394a1511
Marcin <walczakmarcin73@gmail.com>
2022-05-21 14:51:39 +0200

    Added array articles with img

---
+++ main.js
@@ -0,0 +1,14 @@
+const articles = [
+    {
+        title:'Pierwszy tekst',
+        image: 'https://cdn.pixabay.com/photo/2020/02/25/09/57/road-4878453_960_720.jpg',
+    },
+    {
+        title:'Drugi Tekst',
+        image: 'https://cdn.pixabay.com/photo/2022/04/20/06/28/flowers-7144467_960_720.jpg',
+    },
+    {
+        title:'Tytuł Trzeci',
+        image: 'https://cdn.pixabay.com/photo/2021/05/12/10/09/common-blue-6247877_960_720.jpg',
+    }
+]
